31f7dcc8ccecfad94992cae7d864296417117555
Yurii Horchuk <yurii.horchuk@gmail.com>
2018-09-24 17:31:28 +0300

    it was added header and sidebar

---
+++ resources/views/admin/includes/head.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="{{asset('css/admin/header.css')}}">
+<link rel="stylesheet" href="{{asset('css/admin/sidebar.css')}}">
+<link rel="stylesheet" href="{{asset('css/admin/footer.css')}}">
---
+++ resources/views/admin/includes/header.blade.php
@@ -0,0 +1,11 @@
+<ul id='dropdown1' class='dropdown-content'>
+    <li><a href="#">Sign out</a></li>
+</ul>
+<nav>
+    <div class="nav-wrapper">
+        <a href="#" class="brand-logo"><img class="__image-logo" src="{{asset('images/logo.png')}}" alt=""></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a class="dropdown-trigger" href="#" data-target="dropdown1">User Abramian<i class="material-icons right">arrow_drop_down</i></a></li>
+        </ul>
+    </div>
+</nav>
