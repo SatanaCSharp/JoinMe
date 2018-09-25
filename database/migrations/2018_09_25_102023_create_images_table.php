f449aa02dcef30a10f76794db0e7f3c52d6760bf
Yurii Horchuk <yurii.horchuk@gmail.com>
2018-09-25 17:17:28 +0300

    It was created migrations

---
+++ database/migrations/2018_09_25_102023_create_images_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('image');
+            $table->timestamps();
+        });
+        Schema::table('users',function (Blueprint $table){
+            $table->integer('image_id')->unsigned();
+            $table->foreign('image_id')->references('id')->on('images');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table)
+        {
+            $table->dropForeign(['image_id']);
+        });
+        Schema::dropIfExists('images');
+    }
+}
---
+++ database/migrations/2018_09_25_102104_create_addresses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('city_street');
+            $table->string('place');
+            $table->timestamps();
+        });
+        Schema::table('users',function (Blueprint $table){
+            $table->integer('address_id')->unsigned();
+            $table->foreign('address_id')->references('id')->on('addresses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table)
+        {
+            $table->dropForeign(['address_id']);
+        });
+        Schema::dropIfExists('addresses');
+    }
+}
